449e493eb5f4d3eb39e476e71cb997cd352403ef
gabriel <gabbade@gmail.com>
2019-10-26 20:44:00 -0300

    enviaEmail

--- hackathonlasalle/hackathonlasalle/Controllers/HomeController.cs
+++ hackathonlasalle/hackathonlasalle/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace hackathonlasalle.Controllers
 {
@@ -10,9 +13,59 @@ namespace hackathonlasalle.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            try
+            {
 
+                MqttClient c = new MqttClient("3.83.148.83");
+
+                string clientId = "";
+
+                c.Connect(clientId);
+
+                c.MqttMsgPublishReceived += Client_recievedMessage;
+
+
+                c.Subscribe(new String[] { "hackaton/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+      
             return View();
         }
+
+        static void Client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            //Handle message received
+            var message = System.Text.Encoding.Default.GetString(e.Message);
+            System.Console.WriteLine("Message received: " + message);
+            enviarEmail(new string[] { "gabbade@gmail.com", "fabio.barreto@gmail.com", "mario.joao@lasalle.org.br", "marcia.sadok@lasalle.org.br" });
+        }
+        static void enviarEmail(string[] emailTo)
+        {
+            string userName = "automatico@mobvendas.com.br";
+            string senha = "mobvendas305503";
+            string host = "email-ssl.com.br";
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient(host);
+            mail.IsBodyHtml = true;
+            mail.From = new MailAddress("automatico@mobvendas.com.br");
+            foreach(var emailusuario in emailTo)
+            {
+                mail.To.Add(emailusuario);
+            }
+
+            mail.Subject = "60+CUIDADOS ALERTA";
+            mail.Body = "ALERTA!<br> O senhor Dã Lael saiu do perímetro seguro, verifique sua localização!";
+            SmtpServer.Port = 587;
+
+            SmtpServer.Credentials = new System.Net.NetworkCredential(userName, senha);
+            SmtpServer.EnableSsl = false;
+            SmtpServer.Send(mail);
+        }
+
     }
 }
---
+++ hackathonlasalle/hackathonlasalle/Models/Arduino.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+namespace hackathonlasalle.Models
+{
+    public class Arduino
+    {
+        public string clientId = "1";
+        public MqttClient c = new MqttClient("3.83.148.83");  
+        public Arduino()
+        {
+            c.Connect(clientId);
+            c.MqttMsgPublishReceived += Client_recievedMessage;
+            c.Subscribe(new String[] { "mandarEmail/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+        //public void MandarMsg()
+        //{
+        //    c.MqttMsgPublished += client_MqttMsgPublished;
+
+        //    ushort msgId = c.Publish("/addAlarme", // topic
+        //                   Encoding.UTF8.GetBytes("true"), // message body
+        //                   MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
+        //                   false); // retained
+       
+        //}
+   
+        static void Client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            //Handle message received
+            var message = System.Text.Encoding.Default.GetString(e.Message);
+            System.Console.WriteLine("Message received: " + message);
+
+        }
+    }
+}
